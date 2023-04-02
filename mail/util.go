c5ab6c5ead6c35dc04b8d77d833bc436ea197784
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-02 12:14:14 -0700

    Create util.go

---
+++ mail/util.go
@@ -0,0 +1,9 @@
+package cxnetmail
+
+var (
+  emailRegex = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+)
+
+func IsNormalEmailValid(e string) bool {
+  return emailRegex.MatchString(e)
+}
