1833c9259f3f34e0518bbb857696fff24358bbca
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-08 22:16:05 -0700

    Update util.go

--- ip/util.go
+++ ip/util.go
@@ -1,46 +1,39 @@
 package cxnetip
-import (
-	"net"
-	"bytes"
-)
 
-func AppendSortedIPNetSlices(sorted *[]*net.IPNet, ipnet *net.IPNet) {
-    if len(*sorted) == 0 {
-        *sorted = append(*sorted, ipnet)
-        return
-    }
+func AppendSortedIPPrefixSlices(sorted *[]netip.IPPrefix, ipPrefix netip.IPPrefix) {
+	if len(*sorted) == 0 {
+		*sorted = append(*sorted, ipPrefix)
+		return
+	}
 
-    var left, right, mid int
-    left = 0
-    right = len(*sorted) - 1
+	var left, right, mid int
+	left = 0
+	right = len(*sorted) - 1
 
-    for left <= right {
-        mid = left + (right-left)/2
-        if (*sorted)[mid].Contains(ipnet.IP) {
-            return
-        } else if bytes.Compare((*sorted)[mid].IP, ipnet.IP) < 0 {
-            left = mid + 1
-        } else {
-            right = mid - 1
-        }
-    }
+	for left <= right {
+		mid = left + (right-left)/2
+		if (*sorted)[mid].Contains(ipPrefix.IP()) {
+			return
+		} else if (*sorted)[mid].IP().Less(ipPrefix.IP()) {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
 
-    // Append the new IPNet to the end of the slice
-    *sorted = append(*sorted, nil)
+	// Append the new IPPrefix to the end of the slice
+	*sorted = append(*sorted, netip.IPPrefix{})
 
-    // Shift the elements to the right of the insertion point one position to the right
-    copy((*sorted)[left+1:], (*sorted)[left:len(*sorted)-1])
+	// Shift the elements to the right of the insertion point one position to the right
+	copy((*sorted)[left+1:], (*sorted)[left:len(*sorted)-1])
 
-    // Insert the new IPNet at the insertion point
-    (*sorted)[left] = ipnet
+	// Insert the new IPPrefix at the insertion point
+	(*sorted)[left] = ipPrefix
 }
 
-func IsPrivateSubnet(ipAddress net.IP) uint8 {
-	// my use case is only concerned with ipv4 atm
-	if ipCheck := ipAddress.To4(); ipCheck != nil {
-		// iterate over all our ranges
+func IsPrivateSubnet(ipAddress netip.Addr) uint8 {
+	if ipCheck := ipAddress.As16(); ipCheck != netip.Addr{} {
 		for _, r := range PrivateRanges {
-			// check if this ip is in a private range
 			if InRange(r, ipAddress) == 1 {
 				return 1
 			}
@@ -49,9 +42,8 @@ func IsPrivateSubnet(ipAddress net.IP) uint8 {
 	return 0
 }
 
-func InRange(r IpRange, ipAddress net.IP) uint8 {
-	// strcmp type byte comparison
-	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) < 0 {
+func InRange(r IpRange, ipAddress netip.Addr) uint8 {
+	if ipAddress.Compare(r.start) >= 0 && ipAddress.Compare(r.end) < 0 {
 		return 1
 	}
 	return 0
@@ -59,41 +51,22 @@ func InRange(r IpRange, ipAddress net.IP) uint8 {
 
 var PrivateRanges = []IpRange{
 	IpRange{
-		start: net.ParseIP("10.0.0.0"),
-		end:   net.ParseIP("10.255.255.255"),
+		start: netip.ParseAddr("10.0.0.0"),
+		end:   netip.ParseAddr("10.255.255.255"),
 	},
-	/*
-		IpRange{
-			start: net.ParseIP("100.64.0.0"),
-			end:   net.ParseIP("100.127.255.255"),
-		},
-	*/
 	IpRange{
-		start: net.ParseIP("172.16.0.0"),
-		end:   net.ParseIP("172.31.255.255"),
+		start: netip.ParseAddr("172.16.0.0"),
+		end:   netip.ParseAddr("172.31.255.255"),
 	},
-	/*
-		IpRange{
-			start: net.ParseIP("192.0.0.0"),
-			end:   net.ParseIP("192.0.0.255"),
-		},
-	*/
 	IpRange{
-		start: net.ParseIP("192.168.0.0"),
-		end:   net.ParseIP("192.168.255.255"),
+		start: netip.ParseAddr("192.168.0.0"),
+		end:   netip.ParseAddr("192.168.255.255"),
 	},
-	/*
-		IpRange{
-			start: net.ParseIP("198.18.0.0"),
-			end:   net.ParseIP("198.19.255.255"),
-		},
-	*/
 }
 
-//IpRange - a structure that holds the start and end of a range of ip addresses
 type IpRange struct {
-	start net.IP
-	end   net.IP
+	start netip.Addr
+	end   netip.Addr
 }
 
 func ListContainsIP(ipList []netip.IPPrefix, ip netip.Addr) uint8 {
